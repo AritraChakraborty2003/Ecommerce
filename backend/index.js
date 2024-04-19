3d762573fcd626c2aa0c7baceb9b392c2bf72d5a
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-04-19 20:47:37 +0530

    new update

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,42 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
-
-app.get("/", (req, res) => {
-  const data = {
-    name: "Aritra",
-    age: 21,
-    degree: "B.Tech",
-  };
-  res.end(JSON.stringify(data));
+const ecomBookSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { collection: "Books" }
+);
+mongoose.connect("mongodb://localhost:27017/ecom");
+const books = mongoose.model("Books", ecomBookSchema);
+app.get("/booksAPI", (req, res) => {
+  books
+    .find()
+    .then((apikey) => {
+      res.json(apikey);
+    })
+    .catch((err) => console.log(err));
 });
+app.post("/booksAPI", (req, res) => {});
 app.listen("8000", () => {
   console.log("");
 });
